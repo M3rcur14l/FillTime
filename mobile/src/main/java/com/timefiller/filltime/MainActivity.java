36c60277587816279ee8bbac42502dd4c63de8ab
ChaiBotta <chai.botta@mail.polimi.it>
2015-11-28 14:37:58 +0100

    chai casino

--- mobile/src/main/java/com/timefiller/filltime/MainActivity.java
+++ mobile/src/main/java/com/timefiller/filltime/MainActivity.java
@@ -1,7 +1,7 @@
 package com.timefiller.filltime;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
