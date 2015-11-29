febe71c37668e755c5dff4d97eea925503f0c839
ChaiBotta <chai.botta@mail.polimi.it>
2015-11-29 09:19:48 +0100

    qr code working

--- mobile/src/main/java/com/timefiller/filltime/SplashScreenActivity.java
+++ mobile/src/main/java/com/timefiller/filltime/SplashScreenActivity.java
@@ -1,21 +1,32 @@
 package com.timefiller.filltime;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseLongArray;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static final int PAUSE_TIME = 3000;
+    private static final int PAUSE_TIME = 5000;
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        logo = (ImageView) findViewById(R.id.logo);
+        logo.setBackgroundResource(R.drawable.logo_animation);
+        AnimationDrawable frameAnimation = (AnimationDrawable) logo.getBackground();
+
+        // Start the animation (looped playback by default).
+        frameAnimation.start();
 
 
     }
