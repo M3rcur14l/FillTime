98296e44d48be5cb745271a656abde59471fbb29
antonello <m3rcur14l@hotmail.com>
2015-11-29 01:22:10 +0100

    styles

---
+++ mobile/src/main/java/com/timefiller/filltime/CultureElementActivity.java
@@ -0,0 +1,13 @@
+package com.timefiller.filltime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CultureElementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_culture_element);
+    }
+}
