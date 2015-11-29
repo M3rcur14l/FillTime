d73de83608a7af449177e40ff9bfa6ad04d5fb50
antonello <m3rcur14l@hotmail.com>
2015-11-29 08:18:43 +0100

    game fix

--- mobile/src/main/java/com/timefiller/filltime/GameActivity.java
+++ mobile/src/main/java/com/timefiller/filltime/GameActivity.java
@@ -5,20 +5,27 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import java.util.Random;
+
 public class GameActivity extends AppCompatActivity {
 
-    private String GameURL = "http://play.esviji.com";
+    private String[] gameUrlList = new String[]{
+            "http://play.esviji.com"
+    };
     private WebView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        int random = new Random().nextInt(gameUrlList.length);
+
         setContentView(R.layout.activity_game);
         gameView = (WebView) findViewById(R.id.gameView);
         WebSettings webSettings = gameView.getSettings();
 
         webSettings.setJavaScriptEnabled(true);
-        gameView.loadUrl(GameURL);
+
+        gameView.loadUrl(gameUrlList[random]);
     }
 }
